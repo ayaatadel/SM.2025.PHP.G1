ec0849517bf5f8142159fd30058d6a3645314363
ayaatadel <ayaatadel128@gmail.com>
2025-07-31 12:59:24 +0300

    day5

--- laravel/demo/routes/web.php
+++ laravel/demo/routes/web.php
@@ -1,77 +1,26 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TrackController;
+use App\Http\Middleware\TestToken;
 use Illuminate\Support\Facades\Route;
-// Route:method ('url,(){})
+
 Route::get('/', function () {
     return view('welcome');
 });
-// Route::get('/users', function () {
-//     $users =
-//         [
 
-//             [
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified','token'])->name('dashboard');
 
-//                 "id" => 1,
-//                 "name" => "php",
-//                 "group" => 1
-//             ],
-//             [
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-//                 "id" => 2,
-//                 "name" => "python",
-//                 "group" => 3
-//             ],
-//             [
-
-//                 "id" => 3,
-//                 "name" => "react",
-//                 "group" => 2
-//             ],
-
-//         ];
-//           //      viewName ==> Data
-//     return view('users', ['users' => $users]);
-// });
-// Route::get('/users/{id}', function ($id) {
-//     $users =
-//         [
-
-//             [
-
-//                 "id" => 1,
-//                 "name" => "php",
-//                 "group" => 1
-//             ],
-//             [
-
-//                 "id" => 2,
-//                 "name" => "python",
-//                 "group" => 3
-//             ],
-//             [
-
-//                 "id" => 3,
-//                 "name" => "react",
-//                 "group" => 2
-//             ],
-
-//         ];
-//     if ($id < count($users)) {
-//                       // viewName  ==> Data
-//         return  view('userData',['user'=>$users[$id]]);
-//     } else {
-//         return view('error');
-//         // return abort(404);
-//     }
-// })->where('id','[0-9]+');
-
-///====================================>
-
-// Route::method('url',[ControllerName::class,'function']);
-// Route::get('/students',[StudentController::class,'getStudents'])->name();
-// Route::get('/students/{id}',[StudentController::class,'getStudentData']);
+require __DIR__.'/auth.php';
 Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
 Route::post('/student/store',[StudentController::class,'store'])->name('students.store');
 Route::get('/students', [StudentController::class, 'index'])->name('students.index');
@@ -79,28 +28,29 @@ Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('st
 Route::get('/students/{id}', [StudentController::class, 'show'])->name('students.view');
 Route::get('/students/update/{id}',[StudentController::class,'update'])->name('students.update');
 Route::put('/students/edit/{id}',[StudentController::class,'edit'])->name('students.edit');
-// ==============================>
-//route::resource
 Route::resource('tracks', TrackController::class);
-/**
- *  POST            _ignition/update-config .. ignition.updateConfig › Spatie\LaravelIgnition › UpdateConfigController
- *  POST            student/store ........................................... students.store › StudentController@store
- * GET|HEAD        students ................................................ students.index › StudentController@index
- * GET|HEAD        students/create ....................................... students.create › StudentController@create
- * PUT             students/edit/{id} ........................................ students.edit › StudentController@edit
- * GET|HEAD        students/update/{id} .................................. students.update › StudentController@update
- * DELETE          students/{id} ....................................... students.destroy › StudentController@destroy
- * GET|HEAD        students/{id} ............................................. students.view › StudentController@show
- * =================================== TRack Routes ============================
- *  method of route  url                            name            function in controller
- * GET|HEAD        tracks ..................... tracks.index › TrackController@index
- * POST            tracks ...................................................... tracks.store › TrackController@store
- * GET|HEAD        tracks/create ............................................. tracks.create › TrackController@create
- * GET|HEAD        tracks/{track} ................................................ tracks.show › TrackController@show
- * PUT|PATCH       tracks/{track} ............................................ tracks.update › TrackController@update
- * DELETE          tracks/{track} .......................................... tracks.destroy › TrackController@destroy
- * GET|HEAD        tracks/{track}/edit ........................................... tracks.edit › TrackController@edit
- * GET|HEAD        up ...............................................................................................
 
 
- */
+//1
+// Route::get('/students', [StudentController::class, 'index'])->name('students.index')->middleware("auth");
+//2
+// Route::middleware('auth')->group(function(){
+//  Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
+// Route::post('/student/store',[StudentController::class,'store'])->name('students.store');
+// Route::get('/students', [StudentController::class, 'index'])->name('students.index');
+// Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
+// Route::get('/students/{id}', [StudentController::class, 'show'])->name('students.view');
+// Route::get('/students/update/{id}',[StudentController::class,'update'])->name('students.update');
+// Route::put('/students/edit/{id}',[StudentController::class,'edit'])->name('students.edit');
+// Route::resource('tracks', TrackController::class);
+// });
+
+//3  generate your middleware ( php artisan make:middleware Test)
+// Route::get('/students', [StudentController::class, 'index'])->name('students.index')->middleware(TestToken::class);
+Route::get('/students', [StudentController::class, 'index'])->name('students.index')->middleware("token");
+// Route::get('/test', function () {
+//     return "test middle ware";
+// })->middleware('token');
+Route::get('/test', function () {
+    return view('test');
+});
